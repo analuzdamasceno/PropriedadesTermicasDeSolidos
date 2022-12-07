26e6957d6cfc8403c559339deda688ffb9ae249a
analuzdamasceno <110113014+analuzdamasceno@users.noreply.github.com>
2022-12-06 22:26:45 -0300

    Update extracaoDados.py

--- extracaoDados.py
+++ extracaoDados.py
@@ -1,3 +1,4 @@
+#função para leitura e extração dos dados desejados (as frequências de vibração e as densidades de estado)
 def ler_dados(lista_omega, lista_densidade, arquivo):
     for linha in arquivo:
         omega = linha.split()[0]
@@ -9,17 +10,19 @@ def ler_dados(lista_omega, lista_densidade, arquivo):
         lista_omega.append(omega)
         lista_densidade.append(densidade_estados)
 
+#para o Silício
 arquivo_si = open('dadosSilicio.txt', 'r')
 omega_si = []
 densidade_estados_si = []
 ler_dados(omega_si, densidade_estados_si, arquivo_si)
 
+#Para o GaN
 arquivo_gan = open('dadosGaN.txt')
 omega_gan = []
 densidade_estados_gan = []
 ler_dados(omega_gan, densidade_estados_gan, arquivo_gan)
 
-
+#Para o ZnO (rocksalt)
 arquivo_zno_rocksalt = open("dadosZnO_rocksalt.txt")
 omega_zno_rs = []
 densidade_estados_zno_rs = []
