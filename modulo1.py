bf5e78afe788580a47422c133eed1507ee02107b
analuzdamasceno <110113014+analuzdamasceno@users.noreply.github.com>
2022-12-06 22:24:52 -0300

    Update modulo1.py

--- modulo1.py
+++ modulo1.py
@@ -1,11 +1,10 @@
 import numpy as np
-import matplotlib.pyplot as plt 
-from extracaoDados import*
 
 #Definição das constantes
 kb = 1.380649e-23
 hbar = 1.05457182e-34
 
+#funcao para calculo da capacidade térmica no modelo de Einstein
 def cv_einstein(omega,N,intervaloTemp):
     #energia dos fônons em um cristal para o modelo de Einstein
     def u_einstein(N,omega,T):
@@ -17,21 +16,3 @@ def cv_einstein(omega,N,intervaloTemp):
         cv = (u_einstein(N, omega, (temp)+h) - u_einstein(N, omega, (temp)-h))/(2*h)
         c_einstein.append(cv)
     return c_einstein
-    
-
-
-#calculando a média para usar nesse modelo
-
-omega_si_einstein = sum(omega_si)/len(omega_si)
-
-#calculando a media
-omega_gan_einstein = sum(omega_gan)/(len(omega_gan))
-
-omega_zno_rs_einstein = sum(omega_zno_rs)/len(omega_zno_rs)
-
-temperaturas = np.linspace(0.01, 1000, 5000)
-cv_si_einstein = cv_einstein(omega_si_einstein, 6.02e23, temperaturas)
-cv_gan_einstein = cv_einstein(omega_gan_einstein, 2*6.02e23, temperaturas)
-#plt.plot(temperaturas, cv_si_einstein, label = 'Silício Einstein')
-#plt.grid()
-#plt.show()
