44e79f3c6a3b414dd261d2da80d7aba9d057e267
analuzdamasceno <110113014+analuzdamasceno@users.noreply.github.com>
2022-12-06 22:25:16 -0300

    Add files via upload

---
+++ extracaoDados.py
@@ -0,0 +1,27 @@
+def ler_dados(lista_omega, lista_densidade, arquivo):
+    for linha in arquivo:
+        omega = linha.split()[0]
+        densidade_estados = linha.split()[1]
+        omega = omega.replace(",",".")
+        densidade_estados = densidade_estados.replace(",",".")
+        omega = float(omega)
+        densidade_estados = float(densidade_estados)
+        lista_omega.append(omega)
+        lista_densidade.append(densidade_estados)
+
+arquivo_si = open('dadosSilicio.txt', 'r')
+omega_si = []
+densidade_estados_si = []
+ler_dados(omega_si, densidade_estados_si, arquivo_si)
+
+arquivo_gan = open('dadosGaN.txt')
+omega_gan = []
+densidade_estados_gan = []
+ler_dados(omega_gan, densidade_estados_gan, arquivo_gan)
+
+
+arquivo_zno_rocksalt = open("dadosZnO_rocksalt.txt")
+omega_zno_rs = []
+densidade_estados_zno_rs = []
+ler_dados(omega_zno_rs, densidade_estados_zno_rs, arquivo_zno_rocksalt)
+
