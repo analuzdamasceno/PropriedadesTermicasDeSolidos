f662d952a6c5a0fd6d4d91e5f3e4c1183e492016
analuzdamasceno <110113014+analuzdamasceno@users.noreply.github.com>
2022-12-17 16:29:16 -0300

    Update extracaoDados.py

--- extracaoDados.py
+++ extracaoDados.py
@@ -9,22 +9,3 @@ def ler_dados(lista_omega, lista_densidade, arquivo):
         densidade_estados = float(densidade_estados)
         lista_omega.append(omega)
         lista_densidade.append(densidade_estados)
-
-#para o Silício
-arquivo_si = open('dadosSilicio.txt', 'r')
-omega_si = []
-densidade_estados_si = []
-ler_dados(omega_si, densidade_estados_si, arquivo_si)
-
-#Para o GaN
-arquivo_gan = open('dadosGaN.txt')
-omega_gan = []
-densidade_estados_gan = []
-ler_dados(omega_gan, densidade_estados_gan, arquivo_gan)
-
-#Para o ZnO (rocksalt)
-arquivo_zno_rocksalt = open("dadosZnO_rocksalt.txt")
-omega_zno_rs = []
-densidade_estados_zno_rs = []
-ler_dados(omega_zno_rs, densidade_estados_zno_rs, arquivo_zno_rocksalt)
-
