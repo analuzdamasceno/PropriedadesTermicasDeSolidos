233cc94f1dad4bc49bb1c2890ecb0fb26f1f5b2d
analuzdamasceno <110113014+analuzdamasceno@users.noreply.github.com>
2022-12-19 10:21:06 -0300

    Update modulo4.py

--- modulo4.py
+++ modulo4.py
@@ -6,18 +6,25 @@ import matplotlib.pyplot as plt
 kb = 1.380649e-23
 hbar = 1.05457182e-34
 
-#função para calcular o tempo entre as colisões entre fônons
+'''função para calcular o tempo entre as colisões entre fônons, que recebe como
+parâametros uma frequência de vibração omega (w), uma temperatura, a velocidade do som
+no sólido e a densidade atômica'''
 def tau(w, temp, v, densidade_atomica):
     theta = hbar*v*((6*densidade_atomica/(np.pi))**(1/3))/(2*kb) #temperatura de Debye
     tau_pe = 2.2e7 * (w**2) #fator relacionado ao espalhamento por causa dos elétrons
     tau_v = 9.16e4 * (w**2) * temp * np.exp(-theta/(3*temp)) #fator relacionado a colisão entre fônons, os chamados processos Umklapp
-    t = ( tau_pe + tau_v)**(-1)
+    t = ( tau_pe + tau_v)**(-1) #tempo calculado a partir dos dois fatores mencionados
     return t 
 
-#função para calcular a condutividade térmica
+'''função para calcular a condutividade térmica, que recebe como parâmetros uma lista com os valores
+das frequências de vibração (omega), uma lista de temperaturas para as quais serão calculadas as condutividades,
+a velocidade do som no sólido e a densidade atômica. A função utiliza a outra função tau definida anteriormente'''
 def condutiv(omega, tempLista, v, densidade_atomica):
+    #função que será integrada para cálculo da condutividade
     def f(x, w, temp):
         return (tau(w, temp, v, densidade_atomica)*(x**4)*np.exp(x)/(np.exp(x)-1)**2)
+    '''cálculo da condutividade a partir da criação de uma lista para x, a partir da lista de omega dada e,
+    depois, integrando a função anterior sobre x para dada temperatura'''
     def k(omega, temp):
         lista_x = []
         for w in omega:
@@ -33,15 +40,8 @@ def condutiv(omega, tempLista, v, densidade_atomica):
             inte += 2*f(lista_x[i], omega[i], temp)
         inte = inte * (h/2)
         return (inte * ((kb**4) * (temp**3))/(2*(np.pi**2)*v*(hbar**3)))
-    lista_k = [] 
+    lista_k = []
+    #cálculo da condutividade para toda a lista de temperaturas dada, gerando também uma lista de condutividades
     for temp in tempLista:
         lista_k.append(k(omega, temp)) 
     return lista_k 
-
-
-
-
-
-
-
-
