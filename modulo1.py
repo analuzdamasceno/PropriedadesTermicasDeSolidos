935435abc4abf49ae681089a0349c4370cefd7f2
analuzdamasceno <110113014+analuzdamasceno@users.noreply.github.com>
2022-12-18 22:54:28 -0300

    Update modulo1.py

--- modulo1.py
+++ modulo1.py
@@ -4,9 +4,9 @@ import numpy as np
 kb = 1.380649e-23
 hbar = 1.05457182e-34
 
-#função que retorna uma lista com as capacidades térmicas calculadas por meio do modelo de Einsteins
-# para cada temperatura dada, calculada a partir de um valor de omega, um número N de átomos 
-# e uma lista com o intervalo de temperatura 
+''' função que retorna uma lista com as capacidades térmicas calculadas por meio do modelo de Einstein
+para cada temperatura dada, calculada a partir de um valor de omega, um número N de átomos 
+e uma lista com o intervalo de temperatura '''
 def cv_einstein(omega,N,intervaloTemp):
     #energia dos fônons em um cristal para o modelo de Einstein
     def u_einstein(N,omega,T):
